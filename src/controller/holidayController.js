6f065a4618621960e40a77f50fcfd1a7024c0cc7
aman <amanshah9712@gmail.com>
2022-09-29 10:36:57 +0530

    hiii

---
+++ src/controller/holidayController.js
@@ -0,0 +1,65 @@
+var acl = require('acl');
+
+var dbInstance = require('../db/conn');
+var prefix;
+
+acl = new acl(new acl.mongodbBackend(dbInstance, prefix));
+
+
+acl.addUserRoles( '6332f64bbeb4e99c5b89864f', 'guest', function(err){
+  console.log("HERE123");
+});
+
+
+const Holiday = require("../model/holiday");
+const holidayController = {}
+
+holidayController.list = async(req, res) => {
+    sess = req.session; 
+    try {
+      const blogs =  await Holiday.find();
+      res.render('holidayListing', {
+        data: blogs,name:sess.name,layout:false
+    });
+      // res.json({ data: blogs, status: "success" });
+    } catch (err) {
+      res.status(500).json({ error: err.message });
+    }
+    // res.render("holidayListing",{name:sess.name,layout:false});
+
+ 
+};
+holidayController.getHoliday=  async(req, res) => {
+    sess = req.session; 
+      res.render("addHoliday",{name:sess.name,layout:false});
+}   
+holidayController.addHoliday=  async(req, res) => {
+
+    try {
+        const addHoliday = new Holiday({
+          holiday_name: req.body.holiday_name,
+          holiday_date: req.body.holiday_date
+        });
+        const Holidayadd  = await addHoliday.save();
+        res.status(201).redirect("/holidayListing");
+      
+    } catch (e) {
+      res.status(400).send(e);
+    }
+
+}   
+holidayController.editHoliday=  async(req, res) => {
+    try {
+      sess=req.session
+       const _id = req.params.id;
+      const studentData = await Holiday.findById(_id);
+      res.render('editHoliday', {
+         name:sess.name,layout:false
+    });
+      // res.json({ data: blogs, status: "success" });
+    } catch (err) {
+      res.status(500).json({ error: err.message });
+    }
+  }
+
+module.exports = holidayController
--- src/router/employee.js
+++ src/router/employee.js
@@ -5,12 +5,21 @@ const Holiday = require("../model/holiday");
 const router =  new express.Router();
 const sessions = require("express-session");
 const employeeController = require('../controller/employeeController')
+const holidayController = require('../controller/holidayController')
+
 const app = express();
  router.use(sessions({
     secret: "thisismysecrctekeyfhrgfgrfrty84fwir767",
     saveUninitialized:true,
     resave: false
 }));
+
+router.get("/holidayListing", holidayController.list);
+router.get("/addHoliday", holidayController.getHoliday);
+router.post("/addHoliday", holidayController.addHoliday);
+router.get("/editHoliday/:id", holidayController.editHoliday);
+
+
 router.get("/", employeeController.login);
   
   router.post("/", async (req, res) => {
@@ -55,38 +64,8 @@ router.get("/", employeeController.login);
     res.render("addEmployee",{name:sess.name,layout:false});
   });
   
-  router.get("/addHoliday", (req, res) => {
-    sess = req.session; 
-    res.render("addHoliday",{name:sess.name,layout:false});
-  });
-  router.post("/addHoliday", async(req, res)=>{
-    try {
-        const addHoliday = new Holiday({
-          holiday_name: req.body.holiday_name,
-          holiday_date: req.body.holiday_date
-        });
-        const Holidayadd  = await addHoliday.save();
-        res.status(201).redirect("/holidayListing");
-      
-    } catch (e) {
-      res.status(400).send(e);
-    }
+ 
 
-  })
-  router.get("/holidayListing", async(req, res)=>{
-    sess = req.session; 
-    try {
-      const blogs =  await Holiday.find();
-      res.render('holidayListing', {
-        data: blogs,name:sess.name,layout:false
-    });
-      // res.json({ data: blogs, status: "success" });
-    } catch (err) {
-      res.status(500).json({ error: err.message });
-    }
-    // res.render("holidayListing",{name:sess.name,layout:false});
-
-  })
 
 
   router.post("/addEmlpoyee", async (req, res) => {
