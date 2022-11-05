e3866a9a72fd508141862b1d4506fae112287e8a
amanshah9712 <53229824+amanshah9712@users.noreply.github.com>
2022-11-05 16:31:31 +0530

    add new

--- seed/permission-seed.js
+++ seed/permission-seed.js
@@ -5,19 +5,17 @@ mongoose.connect('mongodb://0.0.0.0:27017/ems');
 
 var employee = [
 
-
+    new addpermission({
+        permission_name: 'View Holidays',
+        permission_description: 'you can View Holidays',
+    }),
+    
     new addpermission({
         permission_name: 'Add Holiday',
         permission_description: 'you can add holiday',
     }),
-    new addpermission({
-        permission_name: 'create project',
-        permission_description: 'you can create project',
-    }),
-    new addpermission({
-        permission_name: 'add task',
-        permission_description: 'you can create task',
 
+    new addpermission({
         permission_name: 'Edit Holiday',
         permission_description: 'you can edit holiday',
     }),
@@ -26,6 +24,12 @@ var employee = [
         permission_description: 'you can delete holiday',
 
     }),
+
+    new addpermission({
+        permission_name: 'View Roles',
+        permission_description: 'you can View Roles',
+    }),
+    
     new addpermission({
         permission_name: 'Add Role',
         permission_description: 'you can add role',
@@ -38,6 +42,12 @@ var employee = [
         permission_name: 'Delete Role',
         permission_description: 'you can delete role',
     }),
+
+    new addpermission({
+        permission_name: 'View Permissions',
+        permission_description: 'you can View Permissions',
+    }),
+
     new addpermission({
         permission_name: 'Add Permission',
         permission_description: 'you can Add Permission',
@@ -51,9 +61,52 @@ var employee = [
         permission_description: 'you can delete role',
     }),
     new addpermission({
-        permission_name: 'Delete Role',
-        permission_description: 'you can delete role',
+        permission_name: 'View Projects',
+        permission_description: 'you can View Projects',
     }),
+
+    new addpermission({
+        permission_name: 'Add Project',
+        permission_description: 'you can add project',
+    }),
+    new addpermission({
+        permission_name: 'Edit Project',
+        permission_description: 'you can Edit Project',
+    }),
+    new addpermission({
+        permission_name: 'Delete Project',
+        permission_description: 'you can delete Project',
+    }),
+
+    new addpermission({
+        permission_name: 'View Tasks',
+        permission_description: 'you can View Tasks',
+    }),
+
+    new addpermission({
+        permission_name: 'Add Task',
+        permission_description: 'you can Add Tasks',
+    }),
+
+    new addpermission({
+        permission_name: 'Edit Task',
+        permission_description: 'you can Edit Task',
+    }),
+    new addpermission({
+        permission_name: 'Delete Task',
+        permission_description: 'you can Delete Task',
+    }),
+    new addpermission({
+        permission_name: 'Add User Permission',
+        permission_description: 'you can Add User Permission',
+    }),
+    new addpermission({
+        permission_name: 'Add Role Permission',
+        permission_description: 'you can Add ROle Permission',
+    }),
+
+
+
 
 ];
 //save function is asynchronous
