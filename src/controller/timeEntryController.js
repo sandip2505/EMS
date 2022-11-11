e2a47acaee0c61524c7fa1738447bb5e6cc0102b
sandip ganava <110095731+sandip2505@users.noreply.github.com>
2022-11-11 16:04:10 +0530

    add

--- src/controller/timeEntryController.js
+++ src/controller/timeEntryController.js
@@ -7,62 +7,86 @@ const BSON = require('bson');
 const timeEntryController = {}
 
 timeEntryController.getData = async (req, res) => {
-    sess = req.session;
-    const user_id = sess.userData._id
-
-  
+  sess = req.session;
+  const user_id = sess.userData._id
 
 
-        const projectData = await Project.find({ user_id: user_id });
-    res.render("AddtimeEntries", { Data: projectData,username: sess.username, layout: false });
+
+
+  const projectData = await Project.find({ user_id: user_id });
+  res.render("AddtimeEntries", { Data: projectData, username: sess.username, layout: false });
 };
 
 
 
 timeEntryController.getTaskByProject = async (req, res) => {
-    const _id = new BSON.ObjectId(req.params.id);
-    try {
+  const _id = new BSON.ObjectId(req.params.id);
+  try {
 
-        const tasks = await Task.aggregate([
-            { $match: { project_id: _id } },
-            {
-                $lookup:
-                {
-                    from: "projects",
-                    localField: "_id",
-                    foreignField: "task_id",
-                    as: "taskData"
-                }
-            }
-        ]);
-        return res.status(200).json({ tasks });
-    } catch (err) {
-        res.status(500).json({ error: err.message });
-    }
+    const tasks = await Task.aggregate([
+      { $match: { project_id: _id } },
+      {
+        $lookup:
+        {
+          from: "projects",
+          localField: "_id",
+          foreignField: "task_id",
+          as: "taskData"
+        }
+      }
+    ]);
+    return res.status(200).json({ tasks });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
 }
 
 timeEntryController.AddHours = async (req, res) => {
-    try{
+  try {
     const AddHours = new Hours({
-        project_id: req.body.project_id,
-        task_id: req.body.task_id,
-        hours: req.body.hours,
+      project_id: req.body.project_id,
+      task_id: req.body.task_id,
+      hours: req.body.hours,
 
-      });
-      const hoursAdd = await AddHours.save();
-      res.status(201).redirect("/index");
-    } catch (e) {
-      res.status(400).send(e);
-    }
+    });
+    const hoursAdd = await AddHours.save();
+    res.status(201).redirect("/index");
+  } catch (e) {
+    res.status(400).send(e);
+  }
 };
 
 timeEntryController.timeEntryList = async (req, res) => {
-    sess = req.session;
+  sess = req.session;
+  sess = req.session;
   try {
-    const blogs = await Hours.find({ deleted_at: "null" });
-    res.render('timeEntryListing', {
-      data: blogs, username: sess.username, layout: false
-    });
+    const Hoursdata = await Hours.aggregate([
+      { $match: { deleted_at: "null" } },
+      {
+
+        $lookup:
+        {
+          from: "projects",
+          localField: "project_id",
+          foreignField: "_id",
+          as: "test"
+        },
+
+      },
+      {
+        $lookup:
+        {
+          from: "tasks",
+          localField: "task_id",
+          foreignField: "_id",
+          as: "test1"
+        }
+      }
+
+    ]);
+    console.log(Hoursdata);
+    res.render("timeEntryListing", { data: Hoursdata, users: sess.userData, username: sess.username });
+    // res.status(201).redirect("/index");
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
