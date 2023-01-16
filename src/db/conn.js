0762223d8ec512ed220df30384092838ed2e5cbb
sandip ganava <110095731+sandip2505@users.noreply.github.com>
2023-01-16 14:51:00 +0530

    add

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,16 +1,16 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
 
+const conn = process.env.CONNECTION;
 
-const conn = process.env.CONNECTION
-
-mongoose.connect(conn, {
-
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-
-}).then(() => {
-  console.log("connection  is successfull");
-}).catch((e) => {
-  console.log("no connection");
-})
+mongoose
+  .connect(conn, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("connection  is successfull");
+  })
+  .catch((e) => {
+    console.log("no connection");
+  });
