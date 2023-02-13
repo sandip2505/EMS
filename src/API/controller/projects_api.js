3b32310c01260fc42ceb88815a1adaa32e0aaf62
sandip2505 <codecrew0@gmail.com>
2023-02-13 11:42:55 +0530

    add

--- src/API/controller/projects_api.js
+++ src/API/controller/projects_api.js
@@ -2960,9 +2960,6 @@ apicontroller.addxlsxfile = async (req, res, next) => {
   res.redirect("userListing");
 };
 
-
-
-
 apicontroller.checkUserHAsPermission = async (req, res) => {
   // const user_id = req.params.id;
   const role_id = req.user.role_id.toString()
