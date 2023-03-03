4653c3a723e9c0f3990f96a77367c072b30dee24
sandip2505 <codecrew0@gmail.com>
2023-03-03 18:26:08 +0530

    add

--- seed/ems.js
+++ seed/ems.js
@@ -11,7 +11,7 @@ require("dotenv").config();
 var mongoose = require("mongoose");
 mongoose.Promise = global.Promise;
 mongoose.set("strictQuery", false);
-mongoose.connect("mongodb://0.0.0.0:27017/newems");
+mongoose.connect("mongodb://0.0.0.0:27017/ems");
 
 var technology = [
   new addtechnology({
