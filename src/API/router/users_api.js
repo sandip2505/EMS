a2c3cb7905699a62e38044297900f01c0ae638f4
amanshah9712 <amanshah9712@gmail.com>
2023-05-25 13:13:26 +0530

    Token and Leave changes

--- src/API/router/users_api.js
+++ src/API/router/users_api.js
@@ -132,12 +132,12 @@ Apirouter.post("/punch-in" ,checkApiKey, auth, users_api.punch_in);
 Apirouter.post("/punch-out/:id" ,checkApiKey, auth, users_api.punch_out);
 Apirouter.get("/punch_data" ,checkApiKey, auth, users_api.punch_data);
 Apirouter.get("/check_punch" ,checkApiKey, auth, users_api.check_punch);
-Apirouter.get("/ip" , users_api.ip);
 
 
 
 Apirouter.get("/addTimeEntries" ,checkApiKey, auth, users_api.getTimeEntry);
 Apirouter.get("/addWorkingHour" ,checkApiKey, auth, users_api.getAddWorkingHour);
+Apirouter.post("/deleteWorkingHour/:id" ,checkApiKey, auth, users_api.DeleteAddWorkingHour); 
 Apirouter.post("/addWorkingHour" ,checkApiKey, auth, users_api.addWorkingHour);
 Apirouter.get("/editWorkingHour/:id" ,checkApiKey, auth, users_api.editWorkingHour);
 Apirouter.post("/editWorkingHour/:id" ,checkApiKey, auth, users_api.updateWorkingHour);
