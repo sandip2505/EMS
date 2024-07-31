b6e34c0bceaf6f5bc9c66afb087072ee71a9fc6a
Meet Suthar <meet.suthar@codecrewinfotech.com>
2024-07-31 14:07:08 +0530

    Pending timeentry mail

--- src/utils/timeEntryMail.js
+++ src/utils/timeEntryMail.js
@@ -1,12 +1,11 @@
 const nodemailer = require("nodemailer");
 var ejs = require("ejs");
 const { response } = require("express");
-const sendUserEmail = async (firstname , email , date ,timeEntryLink) => {
+const sendUserEmail = async (firstname, email, date, timeEntryLink) => {
   try {
-    console.log("hhahashhhashashuas23",email,date,firstname,timeEntryLink)
     const transporter = nodemailer.createTransport({
       host: process.env.EMAIL_HOST,
-       domain: process.env.EMAIL_DOMAIN,
+      domain: process.env.EMAIL_DOMAIN,
       //  service: "gmail",
       port: 465,
       // secure: true,  
@@ -17,19 +16,19 @@ const sendUserEmail = async (firstname , email , date ,timeEntryLink) => {
     });
     ejs.renderFile(
       "src/views/partials/TimeEtrymail.ejs",
-      { firstname: firstname, date:date , timeEntryLink:timeEntryLink },
+      { firstname: firstname, date: date, timeEntryLink: timeEntryLink },
       (err, data) => {
         if (err) {
           //console.log(err);
         } else {
           transporter.sendMail({
-            from:"no-reply@codecrewinfotech.com",
-            to: 'aman.shah@codecrewinfotech.com',
+            from: "no-reply@codecrewinfotech.com",
+            to: email,
             subject: "Time entry pending",
             text: "codecerw",
             html: data,
           })
-            console.log("email sent sucessfully");
+          console.log("email sent sucessfully");
         }
       }
     );
